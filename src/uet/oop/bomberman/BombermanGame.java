cbba54f7778a926ebaa778f0fa1d75a9ef857168
Kieu Van Tuyen <tuyenkieuvan@gmail.com>
2020-11-09 16:21:09 +0700

    Load map

---
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -0,0 +1,85 @@
+package uet.oop.bomberman;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+import uet.oop.bomberman.entities.Bomber;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.Grass;
+import uet.oop.bomberman.entities.Wall;
+import uet.oop.bomberman.graphics.Sprite;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BombermanGame extends Application {
+    
+    public static final int WIDTH = 31;
+    public static final int HEIGHT = 13;
+    
+    private GraphicsContext gc;
+    private Canvas canvas;
+    private List<Entity> entities = new ArrayList<>();
+    private List<Entity> stillObjects = new ArrayList<>();
+    private LevelLoader instance = LevelLoader.getInstance();
+
+    public static void main(String[] args) {
+        Application.launch(BombermanGame.class);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // Tao Canvas
+        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
+        gc = canvas.getGraphicsContext2D();
+
+        // Tao root container
+        Group root = new Group();
+        root.getChildren().add(canvas);
+
+        // Tao scene
+        Scene scene = new Scene(root);
+
+        // Them scene vao stage
+        stage.setScene(scene);
+        stage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                render();
+                update();
+            }
+        };
+        timer.start();
+
+        createMap();
+
+        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
+        entities.add(bomberman);
+    }
+
+    public void createMap() {
+        Entity[][] map = instance.loadMap(1);
+        for (Entity[] value : map) {
+            for (int j = 0; j < value.length; j++) {
+                stillObjects.add(value[j]);
+            }
+        }
+    }
+
+    public void update() {
+        entities.forEach(Entity::update);
+    }
+
+    public void render() {
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        stillObjects.forEach(g -> g.render(gc));
+        entities.forEach(g -> g.render(gc));
+    }
+}
---
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -0,0 +1,30 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import uet.oop.bomberman.graphics.Sprite;
+
+public abstract class Entity {
+    //T?a ?? X tính t? góc trái trên trong Canvas
+    protected int x;
+
+    //T?a ?? Y tính t? góc trái trên trong Canvas
+    protected int y;
+
+    protected Image img;
+
+    //Kh?i t?o ??i t??ng, chuy?n t? t?a ?? ??n v? sang t?a ?? trong canvas
+    public Entity( int xUnit, int yUnit, Image img) {
+        this.x = xUnit * Sprite.SCALED_SIZE;
+        this.y = yUnit * Sprite.SCALED_SIZE;
+        this.img = img;
+    }
+
+    public void render(GraphicsContext gc) {
+        gc.drawImage(img, x, y);
+    }
+    public abstract void update();
+}
