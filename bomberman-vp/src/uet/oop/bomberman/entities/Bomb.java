e9354072fdbcfcb2fc21af399636f7f3a5f2d65a
Kieu Van Tuyen <tuyenkieuvan@gmail.com>
2020-11-23 11:52:04 +0700

    Add to master branch

---
+++ bomberman-vp/src/uet/oop/bomberman/entities/Bomb.java
@@ -0,0 +1,47 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.BombermanGame;
+import uet.oop.bomberman.Coordinates;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Bomb extends StaticEntity implements Animated {
+
+    protected int timeLeft=120;
+    protected int _animate = 0;
+
+    public Bomb() {
+    }
+
+    public Bomb(Coordinates tile) {
+        super(tile);
+        img= Sprite.bomb.getFxImage();
+    }
+
+    public Bomb(Coordinates tile, Image img) {
+        super(tile, img);
+    }
+
+    @Override
+    public void animate() {
+        if (_animate > 90) _animate = 0;
+        else _animate++;
+        timeLeft--;
+    }
+
+    @Override
+    public void loadAnimated(Sprite sprite1, Sprite sprite2, Sprite sprite3) {
+        img=Sprite.movingSprite(sprite1.getFxImage(),sprite2.getFxImage(),sprite3.getFxImage(),_animate,30);
+    }
+
+    @Override
+    public void update() {
+        animate();
+        if (timeLeft<0){
+            BombermanGame.removeBomb();
+            return;
+        }
+        loadAnimated(Sprite.bomb,Sprite.bomb_1,Sprite.bomb_2);
+
+    }
+}
---
+++ bomberman-vp/src/uet/oop/bomberman/entities/Portal.java
@@ -0,0 +1,26 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.Coordinates;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Portal extends Entity {
+
+    public Portal() {
+        this.img=Sprite.portal.getFxImage();
+    }
+
+    public Portal(Coordinates tile) {
+        super(tile);
+        this.img=Sprite.portal.getFxImage();
+    }
+
+    public Portal(Coordinates tile, Image img) {
+        super(tile, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
